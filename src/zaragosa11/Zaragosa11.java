e20e759eea54390b14422610c64fd48d10e1d827
markzaragosa <johnpaulzaragosa11@gmail.com>
2024-08-28 15:21:28 +0800

    Update Zaragosa11.java

--- src/zaragosa11/Zaragosa11.java
+++ src/zaragosa11/Zaragosa11.java
@@ -5,10 +5,18 @@ public class Zaragosa11 {
 
    
     public static void main(String[] args) {
+        Grades gr = new Grades();
+       
+       
+     System.out.printf("%-10s %-10s %-10.2s 5-10.2s %-10.2s %-10.2s %10-2s 5-10s\n",
+             "studentID", "name", "prelim", "midterm", "prefinal", "finalExam", "average", "remarks");  
         
-        
-        
-        
+       gr.addGrades(1001, "Mike",1.0, 1.0, 1.0, 1.0);
+       gr.viewGrades();
+       
+       Grades grl = new Grades();
+       grl.addGrades(1002, "John", 2.0, 1.0, 1.0, 1.0);
+       grl.viewGrades();
         
     }
     
